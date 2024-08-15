a31bb5969174bf72704f5f21b497b2464789881b
朱宗辉 <1050527083@qq.com>
2024-08-15 22:45:42 +0800

    完成所有数据生成的测试

--- test/RTU_test.go
+++ test/RTU_test.go
@@ -10,12 +10,41 @@ import (
 )
 
 func TestCoilRead(t *testing.T) {
-	read := rtu.CoilRead(0x01, 0x1113, 0x0025)
-	t.Log(read)
+	coilRead := rtu.CoilRead(0x01, 0x1113, 0x0025)
+	t.Logf("%X", coilRead)
+}
+
+func TestDispersedRead(t *testing.T) {
+	dispersedRead := rtu.DispersedRead(0x01, 0x00C4, 0x0016)
+	t.Logf("%X", dispersedRead)
+}
+
+func TestMaintainRead(t *testing.T) {
+	maintainRead := rtu.MaintainRead(0x01, 0x006B, 0x0003)
+	t.Logf("%X", maintainRead)
+}
+
+func TestInputRead(t *testing.T) {
+	inputRead := rtu.InputRead(0x01, 0x0008, 0x0002)
+	t.Logf("%X", inputRead)
+}
+
+func TestCoilWriteOne(t *testing.T) {
+	coilWriteOne := rtu.CoilWriteOne(0x01, 0x00Ac, 0xFF00)
+	t.Logf("%X", coilWriteOne)
+}
+
+func TestMaintainWriteOne(t *testing.T) {
+	maintainWriteOne := rtu.MaintainWriteOne(0x01, 0x0000, 0x0001)
+	t.Logf("%X", maintainWriteOne)
 }
 
 func TestCoilWriteMore(t *testing.T) {
 	coilWriteMore := rtu.CoilWriteMore(0x01, 0x0013, 0x000A, []uint8{0xCD, 0x01})
-	t.Log(coilWriteMore)
+	t.Logf("%X", coilWriteMore)
+}
 
+func TestMaintainWriteMore(t *testing.T) {
+	maintainWriteMore := rtu.MaintainWriteMore(0x01, 0x0001, 0x0002, []uint16{0x000A, 0x0102})
+	t.Logf("%X", maintainWriteMore)
 }

3a96b8d81ef7f4a0818b272ecee5ba9d4c374b69
朱宗辉 <1050527083@qq.com>
2024-08-15 22:11:56 +0800

    实现CRC16 Modbus功能

---
+++ test/CRC_test.go
@@ -0,0 +1,18 @@
+// @Title  CRC_test
+// @Description 测试CEC加密加密相关功能
+// @Author  朱宗辉  2024/8/15 21:51
+// @Update  朱宗辉  2024/8/15 21:51
+package test
+
+import (
+	"fmt"
+	modbus "github.com/thinkgos/gomodbus/v2"
+	"testing"
+)
+
+func TestCRCModbus(t *testing.T) {
+	var buf = []byte{0x01, 0x03, 0x00, 0x00, 0x00, 0x01}
+	crc16 := modbus.CRC16(buf)
+	// 将crc16以16进制方式输出
+	fmt.Println(crc16)
+}

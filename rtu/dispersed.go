c02b58212487fb9f82ff178dad27fa1bf31d4514
朱宗辉 <1050527083@qq.com>
2024-08-15 22:17:05 +0800

    完成读离散输入寄存器数据生成

---
+++ rtu/dispersed.go
@@ -0,0 +1,14 @@
+// @Title  dispersed
+// @Description 生成离散操作相关数据
+// @Author  朱宗辉  2024/8/15 22:14
+// @Update  朱宗辉  2024/8/15 22:14
+package rtu
+
+import "modbus_util/crc"
+
+func DispersedRead(slaveAddr uint8, startAddr uint16, number uint16) []byte {
+	var buf = []byte{slaveAddr, 0x02, byte((startAddr >> 8) & 0xFF), byte(startAddr & 0xFF), byte((number >> 8) & 0xFF), byte(number & 0xFF)}
+	crc16 := crc.CRC16(buf)
+	buf = append(buf, byte(crc16&0xFF), byte((crc16>>8)&0xFF))
+	return buf
+}
